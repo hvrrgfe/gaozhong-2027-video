9e484fb4651fc43f84035833d0ae3d214843983e
我是一个人呐qwedc <133559022+hvrrgfe@users.noreply.github.com>
2026-08-08 19:59:48 +0800

    65s+music

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,4 @@
-import { AbsoluteFill, Sequence } from "remotion";
+import { AbsoluteFill, Audio, Sequence, staticFile } from "remotion";
 import { FPS } from "./theme";
 import { Scene1_Intro } from "./compositions/Scene1_Intro";
 import { Scene2_AboutMe } from "./compositions/Scene2_AboutMe";
@@ -7,25 +7,31 @@ import { Scene4_Activities } from "./compositions/Scene4_Activities";
 import { Scene5_Rankings } from "./compositions/Scene5_Rankings";
 import { Scene6_Ending } from "./compositions/Scene6_Ending";
 
+// 总时长65秒 @ 30fps = 1950帧
+const TOTAL = 65 * FPS;
+
 export const Main: React.FC = () => {
   return (
     <AbsoluteFill style={{ backgroundColor: "#2D1F14" }}>
-      <Sequence from={0 * FPS} durationInFrames={30 * FPS}>
+      {/* 背景音乐（高潮部分） */}
+      <Audio src={staticFile("music_climax.mp3")} />
+
+      <Sequence from={0 * FPS} durationInFrames={16 * FPS}>
         <Scene1_Intro />
       </Sequence>
-      <Sequence from={30 * FPS} durationInFrames={40 * FPS}>
+      <Sequence from={16 * FPS} durationInFrames={8 * FPS}>
         <Scene2_AboutMe />
       </Sequence>
-      <Sequence from={70 * FPS} durationInFrames={40 * FPS}>
+      <Sequence from={24 * FPS} durationInFrames={8 * FPS}>
         <Scene3_Group />
       </Sequence>
-      <Sequence from={110 * FPS} durationInFrames={100 * FPS}>
+      <Sequence from={32 * FPS} durationInFrames={18 * FPS}>
         <Scene4_Activities />
       </Sequence>
-      <Sequence from={210 * FPS} durationInFrames={40 * FPS}>
+      <Sequence from={50 * FPS} durationInFrames={8 * FPS}>
         <Scene5_Rankings />
       </Sequence>
-      <Sequence from={250 * FPS} durationInFrames={110 * FPS}>
+      <Sequence from={58 * FPS} durationInFrames={7 * FPS}>
         <Scene6_Ending />
       </Sequence>
     </AbsoluteFill>
