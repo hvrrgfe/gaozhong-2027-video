2fed7c11b2a8213fb3b22434cbec1bf32a529273
我是一个人呐qwedc <133559022+hvrrgfe@users.noreply.github.com>
2026-08-09 12:15:25 +0800

    老师混剪结构

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,30 +1,29 @@
 import { AbsoluteFill, Audio, Sequence, staticFile } from "remotion";
 import { FPS, theme } from "./theme";
+import { SceneTeachers } from "./compositions/SceneTeachers";
 import { Scene1_Intro } from "./compositions/Scene1_Intro";
 import { Scene3_Group } from "./compositions/Scene3_Group";
 import { Scene4_Activities } from "./compositions/Scene4_Activities";
-import { Scene5_Rankings } from "./compositions/Scene5_Rankings";
 import { Scene6_Ending } from "./compositions/Scene6_Ending";
 
-// 总时长65秒
 export const Main: React.FC = () => {
   return (
     <AbsoluteFill style={{ backgroundColor: theme.mood.bg }}>
       <Audio src={staticFile("music_warm_acoustic.mp3")} volume={0.8} />
 
-      <Sequence from={0 * FPS} durationInFrames={16 * FPS}>
+      <Sequence from={0 * FPS} durationInFrames={28 * FPS}>
+        <SceneTeachers />
+      </Sequence>
+      <Sequence from={28 * FPS} durationInFrames={10 * FPS}>
         <Scene1_Intro />
       </Sequence>
-      <Sequence from={16 * FPS} durationInFrames={10 * FPS}>
+      <Sequence from={38 * FPS} durationInFrames={8 * FPS}>
         <Scene3_Group />
       </Sequence>
-      <Sequence from={26 * FPS} durationInFrames={14 * FPS}>
+      <Sequence from={46 * FPS} durationInFrames={10 * FPS}>
         <Scene4_Activities />
       </Sequence>
-      <Sequence from={40 * FPS} durationInFrames={10 * FPS}>
-        <Scene5_Rankings />
-      </Sequence>
-      <Sequence from={50 * FPS} durationInFrames={15 * FPS}>
+      <Sequence from={56 * FPS} durationInFrames={9 * FPS}>
         <Scene6_Ending />
       </Sequence>
     </AbsoluteFill>
